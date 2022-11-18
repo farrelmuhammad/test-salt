71c72b75260cac465a784030d76870e7e2be6bf9
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-11-18 10:04:52 +0700

    Setup tailwindcss

---
+++ src/components/Box.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Box = () => {
+  return (
+    <>
+      <section className="container">
+        <h1>Box</h1>
+        <div className="flex">
+            
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Box;
